153bfcf3078b4ce0e0e0b7c86c32a62a2fffccd9
YanikaD <yanika.dontong@gmail.com>
2024-02-09 00:33:46 +0700

    add internship

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import './style.css';
-import * as THREE from 'three'; 
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import './style.css';
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+// import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 // Setup
 
